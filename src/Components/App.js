3c319984121d489eef0884baf240c4e0e0698945
Felix Fong <moongod101@hotmail.com>
2018-05-26 03:33:22 +0800

    Add a new Snackbar API

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,8 +3,45 @@ import React from 'react';
 import TitleBar from './TitleBar';
 import Communities from './Communities';
 import FeaturedPosts from './FeaturedPosts';
+import Snackbar from '@material-ui/core/Snackbar';
+import Button from '@material-ui/core/Button';
+import { EventEmitter } from 'fbemitter';
+
+export const SnackbarEmitter = new EventEmitter();
+
 
 export default class App extends React.Component {
+
+    state = {
+        snackbarOpen: false,
+        snackbar: {
+            title: 'Default Title',
+            message: 'Default Text',
+            duration: 2500,
+        }
+    }
+
+    closeSnackbar(){
+        this.setState({
+            snackbarOpen: false,
+        });
+    }
+
+    componentDidMount(){
+        SnackbarEmitter.addListener('push', payload => {
+            console.log(payload);
+            const snackbar = {
+                ...this.state.snackbar,
+                // Overwrite the defaults if available
+                ...payload,
+            };
+            this.setState({
+                snackbar,
+                snackbarOpen: true,
+            });
+        });
+    }
+
     render(){
         return (
             <div id="home_page">
@@ -24,6 +61,29 @@ export default class App extends React.Component {
                 >
                     <FeaturedPosts />
                 </div>
+
+                <Snackbar
+                    anchorOrigin={{ 
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    open={this.state.snackbarOpen}
+                    onClose={() => this.closeSnackbar()}
+                    message={this.state.snackbar.message}
+                    autoHideDuration={this.state.snackbar.duration}
+                    action={
+                        <Button 
+                            color="secondary"
+                            size="small"
+                            onClick={() => this.closeSnackbar()}
+                        >
+                            {
+                                this.state.snackbar.button || 'OK'
+                            }
+                        </Button>
+                    }
+                ></Snackbar>
+
             </div>
         );
     }
