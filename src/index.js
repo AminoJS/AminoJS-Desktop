e60c4f54d26f62539b40bd9039c5ba3fd6dcf289
Felix Fong <moongod101@hotmail.com>
2018-05-28 03:55:36 +0800

    Get the SID when ever the app starts

--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 import './SCSS/index.scss';
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as Amino from 'amino.js';
+(async () => {
+    const email = localStorage.getItem('email');
+    const password = localStorage.getItem('password');
+    if(email && password){
+        const sid = await Amino.login(email, password);
+        console.log(sid);
+        ReactDOM.render(<App />, document.getElementById('root'));
+    }else{
+        ReactDOM.render(<App />, document.getElementById('root'));
+    }
+})();
